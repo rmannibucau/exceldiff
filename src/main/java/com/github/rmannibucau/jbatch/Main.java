196ef280fc0601dfee076ddca06e90325fa96b1a
Romain Manni-Bucau <rmannibucau@gmail.com>
2014-02-23 20:13:49 +0100

    passing parameters in main

--- src/main/java/com/github/rmannibucau/jbatch/Main.java
+++ src/main/java/com/github/rmannibucau/jbatch/Main.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 
 public class Main {
     public static void main(final String[] args) {
-        if (args == null || args.length != 2) {
+        if (args == null || args.length < 2) {
             System.out.println("Usage:\n\njava -jar exceldiff.jar forquart.xls icrh.xlsx");
             return;
         }
@@ -17,6 +17,8 @@ public class Main {
         final long execution = operator.start("diff", new Properties() {{
             setProperty("forquart", args[0]);
             setProperty("icrh", args[1]);
+            if (args.length > 2) setProperty("output", args[2]);
+            if (args.length > 3) setProperty("filter", args[3]);
         }});
 
         Batches.waitForEnd(operator, execution);
